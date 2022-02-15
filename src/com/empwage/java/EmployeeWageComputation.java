cbb185e788c03d8caf17258c9227fc43c0f4c479
rahulbhamre1997 <rahul1997bhamre@gmail.com>
2022-02-15 11:33:53 +0530

    Added:Checking the presenty

--- src/com/empwage/java/EmployeeWageComputation.java
+++ src/com/empwage/java/EmployeeWageComputation.java
@@ -2,8 +2,14 @@ package com.empwage.java;
 
 public class EmployeeWageComputation {
 	public static void main(String[] args) {
-		 
-		System.out.println("Welcome");
-	}
+		int full = 1;	
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+			if (empCheck == full)
+				System.out.println("Employee is Present");
+			else
+				System.out.println("Employee is Absent");
+
+		}
 
 }
+
